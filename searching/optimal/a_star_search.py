ad3f4f9eca5e48d18dd27ad666d1db727cbde83e
Quint Daenen <quint.daenen@student.kuleuven.be>
2018-12-31 09:33:11 +0100

    added a* algorithm w/ iterative deepening

--- searching/optimal/a_star_search.py
+++ searching/optimal/a_star_search.py
@@ -30,8 +30,38 @@ class AStarSearch:
             queue.sort(key=lambda x: Graph.estimate(x, weights, estimate, goal))
         return found
 
+    def iterative_deepening(self, graph, weights, estimate, start, goal, bound):
+        new_bound = 99
+        queue, visited, found = [[start]], set(), []
+        while queue:
+            path = queue.pop(0)
+            visited.add(path[-1])
+            cost = Graph.estimate(path, weights, estimate, goal)
+            if cost > bound:
+                if cost < new_bound:
+                    new_bound = cost
+                continue
+            self.checked.append(path)
+            for child in graph[path[-1]]:
+                if child in visited:
+                    continue
+                if child == goal:
+                    found = path + [child]
+                if not found or \
+                        Graph.estimate(path + [child], weights, estimate, goal) < \
+                        Graph.estimate(found, weights, estimate, goal):
+                    queue.append(path + [child])
+                    for p in queue:
+                        if p[-1] is not (path + [child])[-1]:
+                            continue
+                        if Graph.cost(p, weights) < Graph.cost(path + [child], weights):
+                            queue.remove(path + [child])
+            queue.sort(key=lambda x: Graph.estimate(x, weights, estimate, goal))
+        return found, new_bound
+
 
 def main():
+    print("A* SEARCH")
     ass = AStarSearch()
     path = ass.search(Graph.graph, Graph.weights, Graph.distance, Graph.start, Graph.goal)
 
@@ -52,6 +82,37 @@ def main():
         for i, path in enumerate(ass.left):
             Colors.warning("%s, cost: %0.1f" % (path, Graph.estimate(path, Graph.weights, Graph.distance, Graph.goal)),
                            "\t%.3d" % i)
+
+    print("\nITERATIVE DEEPENING")
+    ass = AStarSearch()
+    path, new_bound = ass.iterative_deepening(Graph.graph, Graph.weights, Graph.distance, Graph.start, Graph.goal, 5)
+
+    while not path:
+        if path:
+            Colors.success(
+                "%s, cost %0.1f" % (path, Graph.estimate(path, Graph.weights, Graph.distance, Graph.goal)), "\tFOUND")
+        else:
+            Colors.fail("[]", "\tNO PATH")
+
+        if ass.checked:
+            print("\tCHECKED:")
+            for i, path in enumerate(ass.checked):
+                Colors.log("%s, cost: %0.1f" % (path, Graph.estimate(path, Graph.weights, Graph.distance, Graph.goal)),
+                           "\t%.3d" % i)
+        print("\n\tBOUND: %d" % new_bound)
+        path, new_bound = ass.iterative_deepening(Graph.graph, Graph.weights, Graph.distance, Graph.start, Graph.goal,
+                                                  new_bound)
+    if path:
+        Colors.success(
+            "%s, cost %0.1f" % (path, Graph.estimate(path, Graph.weights, Graph.distance, Graph.goal)), "\tFOUND")
+    else:
+        Colors.fail("[]", "\tNO PATH")
+
+    if ass.checked:
+        print("\tCHECKED:")
+        for i, path in enumerate(ass.checked):
+            Colors.log("%s, cost: %0.1f" % (path, Graph.estimate(path, Graph.weights, Graph.distance, Graph.goal)),
+                       "\t%.3d" % i)
 
 
 if __name__ == "__main__":
